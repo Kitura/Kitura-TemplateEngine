cc3c90b0682df5c208f6841c148b776a7ef2f0ef
Vadim Eisenberg <vadime@il.ibm.com>
2017-01-20 07:25:15 +0200

    IBM-Swift/Kitura#930 added second render() method with options parameter
    and an extension with its implementation for template engines that
    does not implement it

--- Sources/KituraTemplateEngine/TemplateEngine.swift
+++ Sources/KituraTemplateEngine/TemplateEngine.swift
@@ -42,6 +42,26 @@ public protocol TemplateEngine {
     ///                      the content.
     /// - Parameter context: A set of variables in the form of a Dictionary of
     ///                     Key/Value pairs, that can be used when generating the content.
+    func render(filePath: String, context: [String: Any]) throws -> String
+
+    /// Take a template file and a set of "variables" in the form of a context
+    /// and generate content to be sent back to the client.
+    ///
+    /// - Parameter filePath: The path of the template file to use when generating
+    ///                      the content.
+    /// - Parameter context: A set of variables in the form of a Dictionary of
+    ///                     Key/Value pairs, that can be used when generating the content.
+    /// - Parameter options: rendering options, different per each template engine
+    ///
     func render(filePath: String, context: [String: Any], options:
-                    RenderingOptions = NullRenderingOptions()) throws -> String
+                    RenderingOptions) throws -> String
+}
+
+extension TemplateEngine {
+    // implementation of render with options parameter for TemplateEngines
+    // that did not implement it
+    func render(filePath: String, context: [String: Any], options:
+                    RenderingOptions) throws -> String {
+        return try render(filePath: filePath, context: context)
+    }
 }

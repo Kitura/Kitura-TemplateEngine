857101c22d49f0293ebdbf9abdeff871ca1bfcf4
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-01-30 14:38:57 +0000

    Updates to swift 4.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
@@ -20,5 +20,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-TemplateEngine"
+    name: "Kitura-TemplateEngine",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KituraTemplateEngine",
+            targets: ["KituraTemplateEngine"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KituraTemplateEngine"
+        )
+    ]
 )
